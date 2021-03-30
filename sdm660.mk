c228f3817f2e0a124ce873938a3372c37c357ef3
Immanuel Raj <immanuelr44@gmail.com>
2021-03-30 23:27:43 +0530

    sdm660-common: Update secure_interface version
    also add build v1.2

--- sdm660.mk
+++ sdm660.mk
@@ -486,6 +486,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     android.hardware.secure_element@1.1 \
+    android.hardware.secure_element@1.2 \
     rild \
     libavservices_minijail.vendor \
     librmnetctl \
